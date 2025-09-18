3b67a811a4fde71cb0c9003cc943698e2139e338
BlackRubick <blackrubick14@gmail.com>
2025-09-18 13:24:18 -0600

    Creando archivos para la conexion con la api

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,17 +10,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-// Bloquear Ctrl+U, F12, Ctrl+Shift+I y clic derecho
 window.addEventListener('keydown', function(e) {
-  // Ctrl+U
   if (e.ctrlKey && e.key.toLowerCase() === 'u') {
     e.preventDefault();
   }
-  // F12
   if (e.key === 'F12') {
     e.preventDefault();
   }
-  // Ctrl+Shift+I
   if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'i') {
     e.preventDefault();
   }
